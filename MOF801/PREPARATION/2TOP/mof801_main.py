4e37de0ce3520d502d8c69e65ba912b8e0251367
sc-enigma <153753211+sc-enigma@users.noreply.github.com>
2026-03-02 20:00:09 +0700

    Add files via upload

--- MOF801/PREPARATION/2TOP/mof801_main.py
+++ MOF801/PREPARATION/2TOP/mof801_main.py
@@ -1,9 +1,12 @@
 import sys
 import numpy as np
 import pickle
+import random
+import itertools
 
 sys.path.append('../../../COMPONENTS')
 from atom import Atom, select_atoms, remove_atoms, mol2_to_atoms, count_atoms, shift_atoms, dump_atoms, count_atoms, shift_atoms
+from linker_utils import find_linkers, find_connected_atoms, add_h, add_oh
 from read_utils import read_mol2_file
 from write_utils import write_gro_file, write_mol2_file
 from write_utils import write_atoms, write_bonds, write_angles, write_dihedrals, write_restraints, compose_itp_files
@@ -12,28 +15,47 @@ from params_detector import detect_bonds, detect_angles, detect_dihedrals
 from mof801_utils import remove_extra_oxygens, define_mof801_atom_types, define_mof801_atom_names, check_mof801_atom_names
 from mof801_params import get_mof801_params
 
-# STEP 1. Read data
-# Set lower bound in Mercury calculate packing = 0.0
 a     = 17.8348
 b     = 17.8348
 c     = 17.8348
 alpha = 90.00 / 180 * np.pi
 beta  = 90.00 / 180 * np.pi
 gamma = 90.00 / 180 * np.pi
-bounds_a, bounds_b, bounds_c = [0.0, 2.5], [0.0, 2.5], [0.0, 2.5]
-atoms = mol2_to_atoms(read_mol2_file('__source_mof801_444.mol2'))
-atoms = shift_atoms(atoms, np.array([-4.4587, -4.4587, -4.4587]))
+bounds_a = [0.0, 2.5]
+bounds_b = [0.0, 2.5]
+bounds_c = [0.0, 2.5]
 
-# STEP 2. Define atom types and names
-atoms = define_mof801_atom_types(atoms)
-atoms = define_mof801_atom_names(atoms)
-check_mof801_atom_names(atoms)
+# STEP 1. Preprocess data
+### CASHING START ###
+# atoms = mol2_to_atoms(read_mol2_file('__source_mof801_444.mol2'))
+# atoms = shift_atoms(atoms, np.array([-5.4587, -5.4587, -5.4587]))
+# atoms = define_mof801_atom_types(atoms)
+# atoms = define_mof801_atom_names(atoms)
+# check_mof801_atom_names(atoms)
+# with open('__tmp/atoms_mof801.pickle', 'wb') as handle:
+#     pickle.dump(atoms, handle, protocol=pickle.HIGHEST_PROTOCOL)
+### CASHING END ###
+
+with open('__tmp/atoms_mof801.pickle', 'rb') as handle:
+    atoms = pickle.load(handle)
+    
+# STEP 2. Remove some linkers
+'''
+linkers = find_linkers(atoms, ['Zr', 'O1', 'O2'])
+ids_linkers_to_remove = np.unique([random.randint(0, len(linkers)-1) for _ in range(int(len(linkers) * 0.25))])
+ids_atoms_to_remove = list(itertools.chain.from_iterable([linkers[i] for i in ids_linkers_to_remove]))
+for i in ids_linkers_to_remove:
+    connected_oxygens = find_connected_atoms(atoms, ['O1'], linkers[i])
+    atoms = add_h(atoms, connected_oxygens[0][0], ids_atoms_to_remove)
+    atoms = add_h(atoms, connected_oxygens[1][0], ids_atoms_to_remove)
+    atoms = add_oh(atoms, connected_oxygens[0][1], ids_atoms_to_remove)
+    atoms = add_oh(atoms, connected_oxygens[1][1], ids_atoms_to_remove)
+atoms = remove_atoms(atoms, ids_atoms_to_remove)
+'''
 
 # STEP 3. Write .gro and .mol2 files
-# with open('__tmp/atoms_zif7_lp.pickle', 'wb') as handle:
-#     pickle.dump(atoms, handle, protocol=pickle.HIGHEST_PROTOCOL)
-write_gro_file(atoms, 'mof801.gro', a, b, c, alpha, beta, gamma, bounds_a, bounds_b, bounds_c)
-write_mol2_file(atoms, 'mof801.mol2', a, b, c, alpha, beta, gamma)
+write_gro_file(atoms, 'mof801.gro', 42.5870, 42.5870, 42.5870, alpha, beta, gamma)
+write_mol2_file(atoms, 'mof801.mol2', 42.5870, 42.5870, 42.5870, alpha, beta, gamma)
 
 # STEP 4. Write .itp files
 check_mof801_atom_names(atoms)
